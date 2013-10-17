ef902617cc2eac38224ec28f03fda5d82f624779
Washington Luiz <huoxito@gmail.com>
2013-10-17 11:57:40 -0300

    Fix ENV var check in http basic auth

--- app/controllers/spree/adyen_notifications_controller.rb
+++ app/controllers/spree/adyen_notifications_controller.rb
@@ -19,7 +19,7 @@ module Spree
       # Enable HTTP basic authentication
       def authenticate
         authenticate_or_request_with_http_basic do |username, password|
-          username == ENV['ADYEN_NOTIFY_USER'] && password == ['ADYEN_NOTIFY_PASSWD']
+          username == ENV['ADYEN_NOTIFY_USER'] && password == ENV['ADYEN_NOTIFY_PASSWD']
         end
       end
   end
