ee302ce090a3f4db6264e1e891ffeb5c0e3ba843
Washington Luiz <huoxito@gmail.com>
2013-10-20 22:55:05 -0300

    Use local url for redirect in development
    A fixed redirect url can still be configured for testing in staging

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
-  get "checkout/payment/adyen", to: "adyen_redirect#confirm"
+  get "checkout/payment/adyen", to: "adyen_redirect#confirm", as: :adyen_confirmation
   post "adyen/notify", to: "adyen_notifications#notify"
 end
